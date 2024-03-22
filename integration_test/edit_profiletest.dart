7f0eca4a9fadd5b65fee5f2ae53bbb178dfe5e68
leo zhang <letian.zhang@mail.utoronto.ca>
2024-03-22 19:35:04 -0400

    issues with selecting image

---
+++ integration_test/edit_profiletest.dart
@@ -0,0 +1,58 @@
+import 'package:exchange/common/routes/names.dart';
+import 'package:exchange/pages/mine/view.dart';
+import 'package:exchange/pages/login_pages/view.dart';
+import 'package:exchange/pages/my_profile_page/view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:exchange/main.dart' as app;
+import 'package:exchange/pages/user_profile_page/view.dart';
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group('end-to-end test', () {
+    testWidgets(
+      'navigate to sign-up page and create account successfully',
+          (tester) async {
+            app.main();
+            await tester.pumpAndSettle();
+            Get.toNamed(AppRoutes.mine);
+            await tester.pumpAndSettle(); // 等待导航动画完成
+            await tester.tap(find.text('Login'));
+            await tester.pumpAndSettle(); // 等待导航动画完成
+            // 以下是登录测试，与你之前的代码相同
+            await Future.delayed(const Duration(seconds: 2));
+            await tester.enterText(find.byType(TextField).at(0), 'z39yu@uwaterloo.ca');
+            await Future.delayed(const Duration(seconds: 2));
+            await tester.enterText(find.byType(TextField).at(1), '123456');
+            await Future.delayed(const Duration(seconds: 2));
+            await tester.tap(find.byType(ElevatedButton));
+            await Future.delayed(const Duration(seconds: 2));
+            await tester.pumpAndSettle();
+
+            await Future.delayed(const Duration(seconds: 2));
+
+        //expect(find.byType(MinePage), findsOneWidget);
+        await tester.tap(find.text('My Profile'));
+            await tester.pumpAndSettle(); // 等待导航动画完成
+            await Future.delayed(const Duration(seconds: 2));
+            await tester.tap(find.text('Edit Profile'));
+            await tester.pumpAndSettle(); // 等待导航动画完成
+            await Future.delayed(const Duration(seconds: 2));
+            await tester.tap(find.text('Change Avatar'));
+            await tester.pumpAndSettle(); // Wait for the photo selection screen to appear
+            await Future.delayed(const Duration(seconds: 2));
+            final Finder image = find.byType(Image).at(0); // Adjust the 'at(0)' to select the correct image if necessary
+            await tester.tap(image);
+            await tester.pumpAndSettle();
+            await Future.delayed(const Duration(seconds: 4));
+
+            expect(find.text('Edit Profile'), findsOneWidget);
+
+          },
+    );
+
+    // You can add more tests for failure scenarios here
+  });
+}
