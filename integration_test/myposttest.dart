208ae819e1fe63d3bcbcdb0e2dd42aaa259384d6
leo zhang <letian.zhang@mail.utoronto.ca>
2024-03-23 21:53:55 -0400

    more error

--- integration_test/myposttest.dart
+++ integration_test/myposttest.dart
@@ -8,57 +8,57 @@ import 'package:exchange/main.dart' as app;
 
 void main() {
   IntegrationTestWidgetsFlutterBinding.ensureInitialized();
-
   group('end to end test', () {
     testWidgets(
       'navigate to login screen and verify login with correct username and password',
           (WidgetTester tester) async {
         app.main();
         await tester.pumpAndSettle();
-
-        // 确保使用 GetMaterialApp 或在 app.main() 中正确设置环境
+// 确保使用 GetMaterialApp 或在 app.main() 中正确设置环境
         Get.toNamed(AppRoutes.mine);
         await tester.pumpAndSettle(); // 等待导航动画完成
-
         await tester.tap(find.text('Login'));
         await tester.pumpAndSettle(); // 等待导航动画完成
-
-        // 输入用户名和密码
-        await tester.enterText(find.byType(TextField).at(0), 'test@uwaterloo.ca');
+// 输入用户名和密码
+        await tester.enterText(find.byType(TextField).at(0), 'z39yu@uwaterloo.ca');
         await tester.enterText(find.byType(TextField).at(1), '123456');
-
         await tester.tap(find.byType(ElevatedButton));
         await tester.pumpAndSettle();
         await Future.delayed(const Duration(seconds: 2));
-
-        // 进入我的帖子页面
+// 进入我的帖子页面
         await tester.tap(find.text('My Post'));
         await tester.pumpAndSettle(); // 等待导航动画完成
         await Future.delayed(const Duration(seconds: 2));
-
-        // 查找并点击GridView中的第一个GridTile
+// 查找并点击GridView中的第一个GridTile
         final Finder firstGridTile = find.byType(GridTile).at(0);
         await tester.tap(firstGridTile);
         await tester.pumpAndSettle(); // 等待点击事件处理完毕
+        await Future.delayed(const Duration(seconds: 2));
+// 模拟点击系统返回键的行为
         Get.close(2);
+        await tester.pumpAndSettle();
         await Future.delayed(const Duration(seconds: 2));
-        await Future.delayed(const Duration(seconds: 2));
-
         await tester.tap(find.text('My Stars'));
         await tester.pumpAndSettle(); // 等待导航动画完成
         final Finder secondGridTile = find.byType(GridTile).at(0);
         await tester.tap(firstGridTile);
         await tester.pumpAndSettle();
+        await Future.delayed(const Duration(seconds: 2));
         Get.close(2);
-        await tester.tap(find.text('Completed Orders '));
+        await tester.pumpAndSettle();
+        await Future.delayed(const Duration(seconds: 2));
+        await tester.tap(find.text('Completed Orders'));
         await tester.pumpAndSettle(); // 等待导航动画完成
         final Finder thirdGridTile = find.byType(GridTile).at(0);
         await tester.tap(firstGridTile);
         await tester.pumpAndSettle();
+        await Future.delayed(const Duration(seconds: 2));
         Get.close(2);
+        await tester.pumpAndSettle();
+        await Future.delayed(const Duration(seconds: 2));
+        await tester.pumpAndSettle();
         expect(find.text('Setting'), findsOneWidget);
-
-          },
+      },
     );
   });
 }
