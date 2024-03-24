ec5a9676854e0428ddf97f95bfa5b5438bb12193
xiayu <yxia326@gatech.edu>
2024-03-23 20:35:07 -0400

    finished chat feature

--- lib/pages/message/controller.dart
+++ lib/pages/message/controller.dart
@@ -1,12 +1,6 @@
-import 'package:exchange/common/store/user.dart';
-import 'package:exchange/common/utils/utils.dart';
 import 'package:get/get.dart';
 import 'package:tencent_cloud_chat_sdk/enum/V2TimConversationListener.dart';
-import 'package:tencent_cloud_chat_sdk/models/v2_tim_callback.dart';
 import 'package:tencent_cloud_chat_sdk/models/v2_tim_conversation.dart';
-import 'package:tencent_cloud_chat_sdk/models/v2_tim_conversation_result.dart';
-import 'package:tencent_cloud_chat_sdk/models/v2_tim_user_full_info.dart';
-import 'package:tencent_cloud_chat_sdk/models/v2_tim_value_callback.dart';
 import 'package:tencent_cloud_chat_sdk/tencent_im_sdk_plugin.dart';
 
 import 'index.dart';
@@ -16,53 +10,53 @@ class MessageController extends GetxController {
 
   final state = MessageState();
 
-  Future<void> getConversationList() async {
-    V2TimValueCallback<V2TimConversationResult> res = await TencentImSDKPlugin
-        .v2TIMManager
-        .getConversationManager()
-        .getConversationList(count: 100, nextSeq: "0");
-    state.conversationList = res.data!.conversationList!;
-  }
+  // Future<void> getConversationList() async {
+  //   V2TimValueCallback<V2TimConversationResult> res = await TencentImSDKPlugin
+  //       .v2TIMManager
+  //       .getConversationManager()
+  //       .getConversationList(count: 100, nextSeq: "0");
+  //   state.conversationList = res.data!.conversationList!;
+  // }
 
-  Future<void> updateUserInfo() async {
-    var userId = UserStore.to.customerProfilesDetails['id'];
-    var nickName = UserStore.to.customerProfilesDetails['name'];
-    var avatar = UserStore.to.customerProfilesDetails['avatar'];
+  // Future<void> updateUserInfo() async {
+  //   var userId = UserStore.to.customerProfilesDetails['id'];
+  //   var nickName = UserStore.to.customerProfilesDetails['name'];
+  //   var avatar = UserStore.to.customerProfilesDetails['avatar'];
 
-    V2TimUserFullInfo userFullInfo = V2TimUserFullInfo(
-      nickName: nickName,
-      faceUrl: avatar,
-      userID: userId,
-    );
-    V2TimCallback setSelfInfoRes = await TencentImSDKPlugin.v2TIMManager
-        .setSelfInfo(userFullInfo: userFullInfo); //用户资料设置信息
-    if (setSelfInfoRes.code == 0) {
-      print("setSelfInfo success");
-    }
-  }
+  //   V2TimUserFullInfo userFullInfo = V2TimUserFullInfo(
+  //     nickName: nickName,
+  //     faceUrl: avatar,
+  //     userID: userId,
+  //   );
+  //   V2TimCallback setSelfInfoRes = await TencentImSDKPlugin.v2TIMManager
+  //       .setSelfInfo(userFullInfo: userFullInfo); //用户资料设置信息
+  //   if (setSelfInfoRes.code == 0) {
+  //     print("setSelfInfo success");
+  //   }
+  // }
 
-  Future<void> timLogin() async {
-    var userId = UserStore.to.customerProfilesDetails['id'];
+  // Future<void> timLogin() async {
+  //   var userId = UserStore.to.customerProfilesDetails['id'];
 
-    // request userSig from backend
-    var userSig = await HttpUtil().post(
-      "https://exchange-vwdpdyrqzw.us-east-1.fcapp.run/api/v1/auth/userSig",
-      data: {"userId": userId},
-    );
+  //   // request userSig from backend
+  //   var userSig = await HttpUtil().post(
+  //     "https://exchange-vwdpdyrqzw.us-east-1.fcapp.run/api/v1/auth/userSig",
+  //     data: {"userId": userId},
+  //   );
 
-    V2TimCallback res = await TencentImSDKPlugin.v2TIMManager.login(
-      userID: userId,
-      userSig: userSig["data"],
-    );
+  //   V2TimCallback res = await TencentImSDKPlugin.v2TIMManager.login(
+  //     userID: userId,
+  //     userSig: userSig["data"],
+  //   );
 
-    if (res.code == 0) {
-      print("login success");
-      getConversationList();
-      updateUserInfo();
-    } else {
-      print("login failed");
-    }
-  }
+  //   if (res.code == 0) {
+  //     print("login success");
+  //     getConversationList();
+  //     updateUserInfo();
+  //   } else {
+  //     print("login failed");
+  //   }
+  // }
 
   void handleConversation() async {
     V2TimConversationListener listener = V2TimConversationListener(
@@ -92,7 +86,7 @@ class MessageController extends GetxController {
   void onInit() {
     super.onInit();
 
-    timLogin();
+    // timLogin();
     handleConversation();
   }
 
