fad7dbbb3d2a46f560ebcd98e63b9e2232bd9f55
leo zhang <letian.zhang@mail.utoronto.ca>
2024-04-03 00:13:52 -0400

    bug in star

---
+++ integration_test/star_unstar_test.dart
@@ -0,0 +1,103 @@
+import 'package:exchange/common/routes/names.dart';
+import 'package:exchange/pages/mine/view.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:get/get.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:exchange/main.dart' as app;
+
+void main() {
+  IntegrationTestWidgetsFlutterBinding.ensureInitialized();
+
+  group(
+    'end to end test',
+        () {
+      testWidgets(
+        'navigate to login screen and verify login with correct username and password',
+            (tester) async {
+          app.main();
+          await tester.pumpAndSettle();
+          Get.toNamed(AppRoutes.mine);
+          await tester.pumpAndSettle(); // 等待导航动画完成
+          await tester.tap(find.text('Login'));
+          await tester.pumpAndSettle(); // 等待导航动画完成
+          // 以下是登录测试，与你之前的代码相同
+          await Future.delayed(const Duration(seconds: 2));
+          await tester.enterText(
+              find.byType(TextField).at(0), 'leo@uwaterloo.ca');
+          await Future.delayed(const Duration(seconds: 2));
+          await tester.enterText(find.byType(TextField).at(1), '123456');
+          await Future.delayed(const Duration(seconds: 2));
+          await tester.tap(find.byType(ElevatedButton));
+          await Future.delayed(const Duration(seconds: 2));
+          await tester.pumpAndSettle();
+
+          await Future.delayed(const Duration(seconds: 2));
+          Get.toNamed(AppRoutes.home);
+          await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle();
+          final Finder firstGirdTileFinder = find.byType(GridTile).first;
+// Wait for the navigation animation to complete
+              await tester.tap(firstGirdTileFinder);
+              await tester.pumpAndSettle();
+              await tester.pumpAndSettle();
+              await tester.pumpAndSettle();
+              await tester.pumpAndSettle();
+              await tester.pumpAndSettle();
+              final Finder starIconFinder = find.byIcon(Icons.star);
+              await tester.tap(starIconFinder);
+              await tester.pumpAndSettle();
+              await tester.pumpAndSettle();
+              await tester.pumpAndSettle();
+              await tester.pumpAndSettle();
+              await tester.pumpAndSettle();
+              Get.close(1);
+
+              //final Finder starIconFinder = find.byIcon(Icons.star);
+         // await tester.tap(starIconFinder.first);
+          await tester.pumpAndSettle();
+          await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          Get.toNamed(AppRoutes.mine);
+          await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle(); //await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle();
+          await tester.tap(find.text('My Stars'));
+          await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle(); //await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+          await tester.pumpAndSettle();
+          //final Finder firstGirdTileFinder = find.byType(GridTile).first;
+
+// Tap on the first ListTile
+           await tester.tap(firstGirdTileFinder);
+           await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+            await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+            await tester.pumpAndSettle(); //await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+            await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+            await tester.pumpAndSettle();
+            await tester.tap(starIconFinder);
+            await tester.pumpAndSettle();
+            await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+              await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+              await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+              await tester.pumpAndSettle(); // Wait for the navigation animation to complete
+              await tester.pumpAndSettle();
+
+
+
+
+            },
+      );
+
+      // 对于登录失败的测试，可以采用类似的逻辑进行导航和验证
+    },
+  );
+}
